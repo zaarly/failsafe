4833aba16f8d192a58057a9293aec498b649763c
Alex Sharp <ajsharp@gmail.com>
2012-03-15 16:07:57 -0700

    Add MockFailureBackend for test mode

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ $:.push(File.expand_path(File.dirname(__FILE__)) + '/../lib')
 
 require 'failsafe'
 
+class MockFailureBackend < Failsafe::Backends::Base
+end
+
 RSpec.configure do |config|
   config.mock_with :mocha
   config.after { Failsafe.error_backends.clear }
