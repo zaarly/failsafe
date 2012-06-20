8dc2294ab06b09cfb8ea176b13157af10877730b
Zachary Heller <zheller@gmail.com>
2012-06-20 14:44:58 -0700

    testing exceptional

--- lib/failsafe/failure.rb
+++ lib/failsafe/failure.rb
@@ -1,9 +1,10 @@
 module Failsafe
   module Backends
-    autoload :Base,     'failsafe/backends/base'
+    autoload :Base,        'failsafe/backends/base'
 
-    autoload :Airbrake, 'failsafe/backends/airbrake'
-    autoload :File,     'failsafe/backends/file'
-    autoload :Stderr,   'failsafe/backends/stderr'
+    autoload :Airbrake,    'failsafe/backends/airbrake'
+    autoload :File,        'failsafe/backends/file'
+    autoload :Stderr,      'failsafe/backends/stderr'
+    autoload :Exceptional, 'failsafe/backends/exceptional'
   end
 end
